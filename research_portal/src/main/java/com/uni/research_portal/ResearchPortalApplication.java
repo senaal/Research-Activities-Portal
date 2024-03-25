e07cfbb374e3c00611a7da687bb69a88a22dafac
senaal <fatmasenaalci@gmail.com>
2024-03-25 22:33:12 +0300

    initialize project

---
+++ research_portal/src/main/java/com/uni/research_portal/ResearchPortalApplication.java
@@ -0,0 +1,13 @@
+package com.uni.research_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchPortalApplication.class, args);
+	}
+
+}
---
+++ research_portal/src/test/java/com/uni/research_portal/ResearchPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.research_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
