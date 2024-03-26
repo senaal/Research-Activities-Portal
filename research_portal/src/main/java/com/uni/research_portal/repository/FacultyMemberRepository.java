db19751c11714e35065490e1ac4b192ab828069b
senaal <fatmasenaalci@gmail.com>
2024-03-26 22:12:07 +0300

    External api call

--- research_portal/src/main/java/com/uni/research_portal/repository/FacultyMemberRepository.java
+++ research_portal/src/main/java/com/uni/research_portal/repository/FacultyMemberRepository.java
@@ -3,7 +3,10 @@ package com.uni.research_portal.repository;
 import com.uni.research_portal.model.FacultyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FacultyMemberRepository extends JpaRepository<FacultyMember, Integer> {
+    List<FacultyMember> findByIsDeletedFalse();
 
-
+    FacultyMember findByOpenAlexId(String id);
 }
