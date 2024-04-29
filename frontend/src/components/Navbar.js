bf085abef9a0f0affee581d02ce21ee8acde5ddd
senaal <fatmasenaalci@gmail.com>
2024-04-29 13:13:28 +0300

    feat: navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './photo.jpeg';
+
+function Navbar(){
+    useEffect(() => {
+        const navLinks = document.querySelectorAll('.nav-link');
+    
+        navLinks.forEach(function(link) {
+          link.addEventListener('click', function() {
+            navLinks.forEach(function(navLink) {
+              navLink.classList.remove('active');
+            });
+            this.classList.add('active');
+          });
+        });
+      }, []);
+  return (
+    <div>
+
+    <div className='header'>
+        <div className='unilogo'>
+            <img src={logo} alt="logo" />
+        </div>
+
+        <div className='uniname'>
+            <h1>Bogazici University Research Portal</h1>      
+        </div> 
+    </div>
+   
+      <nav className='navbar'>
+        <div className='nav-box'>
+          <Link to="/department" className='nav-link'>
+            Departments
+          </Link>
+        </div>
+        <div className='nav-box'>
+            <Link to="/profiles" className='nav-link'>
+            Profiles
+          </Link>
+        </div>
+        <div className='nav-box'>
+            <Link to="/scientific-articles" className='nav-link'>
+            Scientific Articles
+          </Link>
+        </div>
+        <div className='nav-box'>
+          <Link to="/projects" className='nav-link'>
+            Projects
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
