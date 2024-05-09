5f929d66d189eb44761e94c3556ba10bb250884a
zeynep-baydemir <zeynepbaydemirr@gmail.com>
2024-05-09 17:16:56 +0300

    create project is added

--- research_portal/src/main/java/com/uni/research_portal/service/DepartmentService.java
+++ research_portal/src/main/java/com/uni/research_portal/service/DepartmentService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class DepartmentService {
     @Autowired
@@ -26,4 +28,5 @@ public class DepartmentService {
         departmentRepository.save(newDepartment);
         return newDepartment;
     }
+
 }
