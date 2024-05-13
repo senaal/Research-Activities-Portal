d8385612c7e246897d9c04fae71185fd858a8fea
zeynep-baydemir <zeynepbaydemirr@gmail.com>
2024-05-14 01:37:58 +0300

    sync triggers and logs

--- research_portal/src/main/java/com/uni/research_portal/ResearchPortalApplication.java
+++ research_portal/src/main/java/com/uni/research_portal/ResearchPortalApplication.java
@@ -2,8 +2,10 @@ package com.uni.research_portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ResearchPortalApplication {
 
 	public static void main(String[] args) {
